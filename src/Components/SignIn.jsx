857f7ad5254a5491130909b61611034c1b6ec19a
arafat22184 <123alarafat@gmail.com>
2025-05-19 04:11:41 +0600

    users login time data update on DB

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { use } from "react";
+import { AuthContext } from "../Context/AuthContext";
 
 const SignIn = () => {
-  return <div></div>;
+  const { signInUser } = use(AuthContext);
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    // Signin with Firebase
+    signInUser(email, password)
+      .then((result) => {
+        console.log(result);
+        const signInInfo = {
+          email,
+          lastSignInTime: result.user?.metadata?.lastSignInTime,
+        };
+        fetch("http://localhost:3000/users", {
+          method: "PATCH",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(signInInfo),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="card bg-base-100 max-w-sm mx-auto shrink-0 shadow-2xl">
+      <div className="card-body">
+        <h1 className="text-5xl font-bold">Sign In now!</h1>
+        <form onSubmit={handleSignIn} className="fieldset">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            name="email"
+            className="input"
+            placeholder="Email"
+          />
+          <label className="label">Password</label>
+          <input
+            type="text"
+            name="password"
+            className="input"
+            placeholder="Password"
+          />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Sign In</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
