56194416f03e0acd36ebc4431703afdac1f71253
arafat22184 <123alarafat@gmail.com>
2025-05-19 00:26:00 +0600

    add coffee data to server

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,30 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import MainLayout from "./layouts/MainLayout.jsx";
+import Home from "./Components/Home.jsx";
+import AddCoffee from "./Components/AddCoffee.jsx";
+import UpdateCoffee from "./Components/UpdateCoffee.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1>Hello World</h1>,
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/addCoffee",
+        Component: AddCoffee,
+      },
+      {
+        path: "/updateCoffee",
+        Component: UpdateCoffee,
+      },
+    ],
   },
 ]);
 
