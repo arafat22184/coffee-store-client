0f3d7cdeb5f9a2bd23f5615c4cbd676dbc8aab62
arafat22184 <123alarafat@gmail.com>
2025-05-19 03:06:02 +0600

    store user data to firebase

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,15 +1,100 @@
-import React from "react";
+import React, { use } from "react";
+import { AuthContext } from "../Context/AuthContext";
+import Swal from "sweetalert2";
 
 const SignUp = () => {
+  const { createUser } = use(AuthContext);
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+
+    const { email, password, ...userProfile } = Object.fromEntries(
+      formData.entries()
+    );
+
+    // Create user in Firebase
+    createUser(email, password)
+      .then((result) => {
+        console.log(result.user);
+        // Save Profile in DB
+        fetch("http://localhost:3000/users", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(userProfile),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.insertedId) {
+              Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "SignUp Successfully",
+                showConfirmButton: false,
+                timer: 1500,
+              });
+            }
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="card bg-base-100 max-w-sm mx-auto shrink-0 shadow-2xl">
       <div className="card-body">
         <h1 className="text-5xl font-bold">Sign Up now!</h1>
-        <form className="fieldset">
+        <form onSubmit={handleSignUp} className="fieldset">
+          <label className="label">Name</label>
+          <input
+            type="text"
+            name="name"
+            className="input"
+            placeholder="Name"
+            autoComplete="true"
+          />
+          <label className="label">Address</label>
+          <input
+            type="text"
+            name="address"
+            className="input"
+            placeholder="Address"
+          />
+
+          <label className="label">Photo URL</label>
+          <input
+            type="text"
+            name="photo"
+            className="input"
+            placeholder="Photo URL"
+          />
+
+          <label className="label">Phone</label>
+          <input
+            type="text"
+            name="phone"
+            className="input"
+            placeholder="Phone Number"
+          />
+
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input
+            type="email"
+            name="email"
+            className="input"
+            placeholder="Email"
+          />
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input
+            type="password"
+            name="password"
+            className="input"
+            placeholder="Password"
+          />
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { AuthContext } from "./AuthContext";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/firebase.init";
 
 const AuthProvider = ({ children }) => {
-  const userInfo = { name: "arafat" };
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const userInfo = { createUser };
 
   return <AuthContext value={userInfo}>{children}</AuthContext>;
 };
